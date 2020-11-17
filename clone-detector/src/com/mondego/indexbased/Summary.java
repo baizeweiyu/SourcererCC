f323ce8aabdbe14d87c21ae331914ebadccff3c8
baizeweiyu <1617377620@qq.com>
2020-11-17 16:51:16 +0800

    format

--- clone-detector/src/com/mondego/indexbased/Summary.java
+++ clone-detector/src/com/mondego/indexbased/Summary.java
@@ -17,87 +17,82 @@ public class Summary {
             throws IOException {
 
         Map<String, String> result = new HashMap<>();
-        result.put("function id", "");
-        result.put("file name and path", "");
+        result.put("function_id", "");
+        result.put("file_name_and_path", "");
         result.put("similarity", ">=" + th + "0%");
         result.put("LOC", "");
-        result.put("total tokens", "");
-        result.put("start line", "");
-        result.put("end line", "");
+        result.put("total_tokens", "");
+        result.put("start_line", "");
+        result.put("end_line", "");
 
         Map<String, Object> fileResult = new HashMap<>();
         float t = Float.parseFloat(th);
-        if ( (t - 8.0 >= 0.0) && (9.0 - t > 0.0) ){
+        if ((t - 8.0 >= 0.0) && (9.0 - t > 0.0)) {
             fileResult.put("type", "type3");
-        }
-        if ( (t - 9.0 >= 0.0) && (10.0 - t > 0.0) ){
+        } else if ((t - 9.0 >= 0.0) && (10.0 - t > 0.0)) {
             fileResult.put("type", "type2");
-        }
-        if ( t - 10.0 == 0.0 ){
+        } else if (t - 10.0 == 0.0) {
             fileResult.put("type", "type1");
         }
         fileResult.put("result", result);
 
         Map<String, Object> detectResult = new HashMap<>();
-        detectResult.put("file name and path", "");
-        detectResult.put("function id", "");
+        detectResult.put("file_name_and_path", "");
+        detectResult.put("function_id", "");
         detectResult.put("LOC", "");
-        detectResult.put("total tokens", "");
-        detectResult.put("start line", "");
-        detectResult.put("end line", "");
-        detectResult.put("file result", fileResult);
+        detectResult.put("total_tokens", "");
+        detectResult.put("start_line", "");
+        detectResult.put("end_line", "");
+        detectResult.put("file_result", fileResult);
 
         List<Map<String, Object>> detectList = new ArrayList<>();
 
         Gson objectGson = new Gson();
         Map<String, Object> object = new HashMap<>();
         object.put("method", "SourcererCC code clone");
-        object.put("user workspace", userPath);
-        object.put("clone detection result", detectList);
+        object.put("user_workspace", userPath);
+        object.put("clone_detection result", detectList);
 
 //        File re = new File(userPath + "NODE/output"+th+".0/blocksclones_index_WITH_FILTER.txt");
 //        BufferedReader br = new BufferedReader(new FileReader(re));
         BufferedReader br = new BufferedReader(new InputStreamReader(
-                new FileInputStream(userPath + "NODE/output"+th+".0/blocksclones_index_WITH_FILTER.txt"), StandardCharsets.UTF_8));
+                new FileInputStream(userPath + "NODE" + File.separator + "output" + th + ".0"
+                        + File.separator + "blocksclones_index_WITH_FILTER.txt"), StandardCharsets.UTF_8));
         String line;
-        while ((line = br.readLine()) != null){
+        while ((line = br.readLine()) != null) {
             String[] fileID = line.split(",");
-            logger.info(line+'y'+fileID[0]+' '+fileID[1]+' '+fileID[2]+' '+fileID[3]);
+            logger.info(line + 'y' + fileID[0] + ' ' + fileID[1] + ' ' + fileID[2] + ' ' + fileID[3]);
 
-            File s = new File(userPath + "file_block_stats/files-stats-0.stats");
+            File s = new File(userPath + "file_block_stats" + File.separator + "files-stats-0.stats");
             BufferedReader fileStats = new BufferedReader(new FileReader(s));
             String file_s;
-            int done = 0;
-            while ((done < 4) && ((file_s = fileStats.readLine()) != null) ){
+            int done = 0while ((done < 4) && ((file_s = fileStats.readLine()) != null)) {
                 String[] status = file_s.split(",");
 
-                if (fileID[1].substring(5,12).equals(status[1])){
+                if (fileID[1].substring(5, 12).equals(status[1])) {
                     status[2] = status[2].replace('"', ' ');
-                    detectResult.put("file name and path", status[2]);
+                    detectResult.put("file_name_and_path", status[2]);
                     done += 1;
-                }
-                else if (fileID[1].equals(status[1])){
-                    detectResult.put("function id", status[1]);
+                } else if (fileID[1].equals(status[1])) {
+                    detectResult.put("function_id", status[1]);
                     detectResult.put("LOC", status[4]);
-                    detectResult.put("start line", status[6]);
-                    detectResult.put("end line", status[7]);
+                    detectResult.put("start_line", status[6]);
+                    detectResult.put("end_line", status[7]);
                     done += 1;
-                }
-                else if (fileID[3].substring(5,12).equals(status[1])){
+                } else if (fileID[3].substring(5, 12).equals(status[1])) {
                     status[2] = status[2].replace('"', ' ');
-                    result.put("file name and path", status[2]);
+                    result.put("file_name_and_path", status[2]);
                     done += 1;
-                }
-                else if (fileID[3].equals(status[1])){
-                    result.put("function id", status[1]);
+                } else if (fileID[3].equals(status[1])) {
+                    result.put("function_id", status[1]);
                     result.put("LOC", status[4]);
-                    result.put("start line", status[6]);
-                    result.put("end line", status[7]);
+                    result.put("start_line", status[6]);
+                    result.put("end_line", status[7]);
                     done += 1;
                 }
             }
             fileResult.put("result", result);
-            detectResult.put("file result", fileResult);
+            detectResult.put("file_result", fileResult);
             detectList.add(detectResult);
             logger.info(detectList);
         }
@@ -105,7 +100,7 @@ public class Summary {
         String jsonString = objectGson.toJson(object);
         System.out.println(jsonString);
 
-        File fj = new File(userPath+"output.json");
+        File fj = new File(userPath + "output.json");
         Writer o = new OutputStreamWriter(new FileOutputStream(fj));
 //        String jsonString = object.toString();
         logger.info(jsonString);
